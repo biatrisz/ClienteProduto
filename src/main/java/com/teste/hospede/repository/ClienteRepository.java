af9e100f7d71553ae97d65ee6b893f9951ee39e5
biatrisz <132359853+biatrisz@users.noreply.github.com>
2024-10-22 11:33:42 -0300

    projetoCliente

---
+++ src/main/java/com/teste/hospede/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.teste.hospede.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teste.hospede.entitie.Cliente;
+
+public interface ClienteRepository extends JpaRepository <Long, Cliente>{
+
+}
